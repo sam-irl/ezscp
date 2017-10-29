de1cba0c4057fd9f751f827d5883c5c2108ae86b
sam-irl <therealsamave@gmail.com>
2017-10-29 16:38:48 -0500

    Install script

---
+++ INSTALL.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/env bash
+echo "ezscp install script"
+echo "checking for root"
+dir=$PWD
+cd /root
+if [ $? -eq 0 ]
+then
+  echo "has root perms!"
+  cd $dir
+else
+  echo "error: script must be run with root permissions, try sudo"
+  cd $dir
+  exit 1
+fi
+
+echo "checking for dependencies"
+if [[ ! $(command -v gzip) == "" ]]
+then
+  echo "gzip found"
+else
+  echo "gzip not located, installing via apt-get"
+  apt-get install gzip
+  if [ $? -eq 0 ]
+  then
+    echo "successfully installed gzip"
+  else
+    echo "problem installing gzip"
+    exit 1
+  fi
+fi
+
+if [[ ! $(command -v scp) == "" ]]
+then
+  echo "scp found"
+else
+  echo "scp not found, installing via apt-get"
+  apt-get install scp
+  if [ $? -eq 0 ]
+  then
+    echo "successfully installed scp"
+  else
+    echo "problem installing scp"
+    exit 1
+  fi
+fi
+
+echo "installing ezscp"
+mv ezscp /usr/bin
+if [ $? -eq 0 ]
+then
+  echo "ezscp installed to /usr/bin"
+else
+  echo "could not move to /usr/bin, do you have write permissions?"
+  exit 1
+fi
+
+chmod +x /usr/bin/ezscp
+if [ $? -eq 0 ]
+then
+  echo "chmod on ezscp successful"
+else
+  echo "could not chmod +x ezscp, do you have permission?"
+  exit 1
+fi
+
+echo "installing manpage"
+gzip ezscp.1
+if [ $? -eq 0 ]
+then
+  echo "gzipped manpage"
+else
+  echo "problem gzipping manpage"
+  exit 1
+fi
+mv ezscp.1.gz /usr/share/man/man1
+if [ $? -eq 0 ]
+then
+  echo "installed manpage"
+else
+  echo "problem moving manpage to /usr/share/man/man1, do you have write permissions?"
+  exit 1
+fi
+echo "script execution successful"
+exit 0
--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # ezscp
+Easy secure copy
+
+## Installation
+
+Requires:
+
+- scp
+
+- bash
+
+- sudo permissions
+
+For install script:
+
+- gzip
+
+To get right to it:
+
+```
+$ chmod +x ./ezscp.sh
+$ ./ezscp.sh
+```
+
+To install to your environment (to run as a command):
+
+For the lazy:
+
+```
+$ chmod +x ./INSTALL.sh
+$ ./INSTALL.sh
+```
+
+Step-by-step install:
+
+```
+$ chmod +x ./ezscp
+$ sudo mv ezscp /usr/bin
+# Optional: to install the manpage
+$ gzip ezscp.1
+$ sudo mv ezscp.1.gz /usr/share/man/man1
+# Run!
+$ ezscp
+```
